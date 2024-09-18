b145258ec84f4cffb1d60619c4c12b97d87be840
Cam Casey <ccasey0313@gmail.com>
2024-09-18 16:01:40 -0600

    finished all exerecises up tp 7

--- 07_tempConversion/tempConversion.js
+++ 07_tempConversion/tempConversion.js
@@ -7,7 +7,7 @@ const convertToFahrenheit = function(celsius) {
   let fahrenheit = (celsius * 9 / 5) + 32;
     return Math.round(fahrenheit * 10) / 10;
 };
-
+ 
 // Do not edit below this line
 module.exports = {
   convertToCelsius,
