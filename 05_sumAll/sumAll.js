f43f6b76f62c2ec2e19804ee33a50679ba0e7b11
Cam Casey <ccasey0313@gmail.com>
2024-09-18 15:51:25 -0600

    finished exerecise 5

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,5 +1,21 @@
-const sumAll = function() {
+const sumAll = function(x, y) {
+    let sum = 0;
+    let smallerNum = x;
+    let largerNum = y;
 
+    if(x < 0 || y < 0 || !Number.isInteger(x) || !Number.isInteger(y)){
+        return 'ERROR';
+    }
+
+    if(x > y){
+        smallerNum = y;
+        largerNum = x;
+    }
+
+    for(let i = smallerNum; i <= largerNum; i++){
+        sum += i;
+    }
+    return sum;
 };
 
 // Do not edit below this line
