cb71b4c33b7eb8c1880c534e9e1d01fba23274df
Cam Casey <ccasey0313@gmail.com>
2024-10-03 18:58:21 -0700

    completed exercise 8

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,5 +1,8 @@
-const palindromes = function () {
+const palindromes = function (str) {
+    let newString = str.toLowerCase(); 
 
+    let reversedStr = newString.split('').reverse().join('');
+    return reversedStr === newString;
 };
 
 // Do not edit below this line
