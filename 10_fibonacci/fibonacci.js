dd6c85b1cee9c82b5f8bfdd81ddd3a4c270b7525
Cam Casey <ccasey0313@gmail.com>
2025-01-04 12:49:13 -0700

    finished exercise 10

--- 10_fibonacci/fibonacci.js
+++ 10_fibonacci/fibonacci.js
@@ -1,5 +1,22 @@
-const fibonacci = function() {
+const fibonacci = function(n) {
+  // Convert string input to a number
+  n = Number(n);
 
+  // Reject negative numbers
+  if (n < 0) return "OOPS";
+
+  // Handle 0 and 1 as base cases
+  if (n === 0) return 0;
+  if (n === 1) return 1;
+
+  let a = 0;
+  let b = 1;
+  for(let i = 2; i <= n; i++){
+    const temp = a + b;
+    a = b;
+    b = temp;
+  }
+  return b;
 };
 
 // Do not edit below this line
